01ed827659079dd3276deabb51f8e053d1bfb5e7
Mide Ojikutu <mide.ojikutu@gmail.com>
2018-09-06 20:37:48 +0100

    rename stuf

--- src/test/scala/com/github/mideo/hashtable/sharding/HashFunctionSpec.scala
+++ src/test/scala/com/github/mideo/hashtable/sharding/HashFunctionSpec.scala
@@ -2,46 +2,46 @@ package com.github.mideo.hashtable.sharding
 
 import java.util.UUID
 
-class HashingSpec extends HashIndexTableSpec {
+class HashFunctionSpec extends HashIndexTableSpec {
   val limit: Int = 100
 
-  object Subject extends Hashing
+  object AHashFunction extends HashFunction
 
   it should "generate a random int within bucket limit for a given object" in {
     val s: String = UUID.randomUUID().toString
 
 
-    withClue(s"${Subject.hash(s, limit)} is less than limit 0") {
-      Subject.hash(s, limit) > 0 should be(true)
+    withClue(s"${AHashFunction(s, limit)} is less than limit 0") {
+      AHashFunction.apply(s, limit) > 0 should be(true)
     }
-    withClue(s"${Subject.hash(s, limit)} is greater than limit $limit") {
-      Subject.hash(s, limit) < limit should be(true)
+    withClue(s"${AHashFunction(s, limit)} is greater than limit $limit") {
+      AHashFunction.apply(s, limit) < limit should be(true)
     }
   }
 
   it should "generate a same int with different limits for a given object" in {
     val s: String = UUID.randomUUID().toString
 
-    withClue(s"${Subject.hash(s, limit)} is not same as ${Subject.hash(s, 200)}") {
-      Subject.hash(s, limit) should equal(Subject.hash(s, 200))
+    withClue(s"${AHashFunction(s, limit)} is not same as ${AHashFunction.apply(s, 200)}") {
+      AHashFunction.apply(s, limit) should equal(AHashFunction.apply(s, 200))
     }
   }
 
 
   it should "always generate the same number for same string" in {
     val s: String = UUID.randomUUID().toString
-    Subject.hash(s, limit) should equal(Subject.hash(s, limit))
+    AHashFunction.apply(s, limit) should equal(AHashFunction(s, limit))
   }
 
 
   it should "generate unique number" in {
-    Subject.hash(UUID.randomUUID().toString, limit) should not equal Subject.hash(UUID.randomUUID().toString, limit)
+    AHashFunction(UUID.randomUUID().toString, limit) should not equal AHashFunction(UUID.randomUUID().toString, limit)
   }
 
   it should "generate unique numbers" in {
     val dataMap: List[Int] =
       (((0 until 1000000) map {
-        _ => Subject.hash(UUID.randomUUID().toString, limit)
+        _ => AHashFunction(UUID.randomUUID().toString, limit)
       } groupBy identity mapValues (_.size)).values toList) sorted
 
     withClue(s"Data not distributed across all buckets") {
